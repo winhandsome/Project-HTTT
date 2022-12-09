1d6e3f261be8a70d1c5e41426e4861916dabeefc
Trong_Nguyen <61712725+lokinguyen04@users.noreply.github.com>
2022-12-09 15:14:48 +0700

    Trong-update-optimization

--- food-order/admin/partials/login-check.php
+++ food-order/admin/partials/login-check.php
@@ -1,5 +1,5 @@
 <?php 
-
+    include('function.php');
     //AUthorization - Access COntrol
     //CHeck whether the user is logged in or not
     if(!isset($_SESSION['user'])) //IF user session is not set
@@ -8,7 +8,7 @@
         //REdirect to login page with message
         $_SESSION['no-login-message'] = "<div class='error text-center'>Please login to access Admin Panel.</div>";
         //REdirect to Login Page
-        header('location:'.SITEURL.'admin/login.php');
+        redirect_to('login.php');
     }
 
 ?>
